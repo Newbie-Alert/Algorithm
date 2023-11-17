10be70a8b93c3560c35d77f57076654e680f16e3
NebieAlert <mjnn100801@naver.com>
2023-11-17 10:06:12 +0900

    내적부터 문자열다루기 기본

--- 제곱근판별.js
+++ 제곱근판별.js
@@ -6,9 +6,9 @@ https://school.programmers.co.kr/learn/courses/30/lessons/12934
 // }
 
 function solution(n) {
-  let root = Math.sqrt(n);
-  var answer = Math.pow(root + 1, 2);
-  if (root % 1 !== 0) {
+  let root = Math.sqrt(n); // 제곱근 구하기
+  var answer = Math.pow(root + 1, 2); // answer는 (제곱근 + 1)을 제곱
+  if (root % 1 !== 0) { // 정수인지 판별하려면 1로 나누었을 때 0 되어야 하는데 아니라면 answer = -1로
     answer = -1;
   }
 
